51dda735de09505a0511d08bc4f78f60952ded06
l0mini <l0mini@naver.com>
2025-05-21 15:49:21 +0900

    feat : 플레이어 이동 및 캠프파이어 데미지구현

---
+++ Assets/Scripts/Player/PlayerCondition.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IDamagable
+{
+    void TakePhysicalDamage(int damageAmount);
+}
+
+public class PlayerCondition : MonoBehaviour, IDamagable
+{
+    public UICondition uiCondition;
+
+    Condition health { get { return uiCondition.health; } }
+    Condition stamina { get { return uiCondition.stamina; } }
+
+    public event Action onTakeDamage;
+
+    private void Update()
+    {
+        stamina.Add(stamina.passiveValue *  Time.deltaTime);
+
+        if(health.curValue == 0f)
+        {
+            Die();
+        }
+    }
+
+    public void Heal(float amount)
+    {
+        health.Add(amount);
+    }
+
+    public void Die()
+    {
+        Debug.Log("Á×À½");
+    }
+
+    public void TakePhysicalDamage(int damageAmount)
+    {
+        health.Subtract(damageAmount);
+        onTakeDamage?.Invoke();
+    }
+}
